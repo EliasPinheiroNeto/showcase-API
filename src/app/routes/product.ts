64a29f90de7d3f97aee7235b975bead3f6b644e4
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-08-11 21:30:18 -0300

    unused folder removed

--- src/app/routes/product.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Express } from 'express'
-import productController from '../controllers/productController'
-
-const router: Router = Router()
-
-router.get('/products', productController.getProducts)
-router.post('/product', productController.createProduct)
-
-export default router
